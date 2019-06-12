3a449d314fcbf80fc2c6ce0ccfad65a4b1b4fac3
Jürgen Buck <j.buck@sap.com>
2019-06-12 14:08:45 +0200

    add get container script

---
+++ getcontainers.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd /etc
+mkdir jumajumo
+cd jumajumo
+
+sudo git clone https://github.com/jumajumo/dht22-docker-arm.git
+sudo git clone https://github.com/jumajumo/relais-button-docker-arm.git
+sudo git clone https://github.com/jumajumo/relais-switch-on-off-docker-arm.git
