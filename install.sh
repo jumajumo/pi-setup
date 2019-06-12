83ce2fca4c6f08904bbf96b9610706982cda0d6e
Jürgen Buck <j.buck@sap.com>
2019-06-12 14:03:25 +0200

    script

--- install.sh
+++ install.sh
@@ -1,2 +1,24 @@
 #!/bin/sh
-echo "here I am"
+
+echo "******************************"
+echo " Setup Docker Support"
+echo "******************************"
+
+curl -sSL https://get.docker.com | sh
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker pi
+
+sudo apt-get install docker-ce=18.06.1~ce~3-0~raspbian
+
+echo "******************************"
+echo " Setup Git Support"
+echo "******************************"
+
+sudo apt-get install git
+
+echo "******************************"
+echo " Restart"
+echo "******************************"
+
+sudo reboot
