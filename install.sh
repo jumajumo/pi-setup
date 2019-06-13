a23947ae939b4ce34b7b8d80a132e61f0525729b
root <root@pidev.fritz.box>
2019-06-13 20:52:32 +0100

    repair install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-echo "******************************"
-echo " Setup Docker Support"
-echo "******************************"
-
 curl -sSL https://get.docker.com | sh
 sudo systemctl enable docker
 sudo systemctl start docker
@@ -11,13 +7,5 @@ sudo usermod -aG docker pi
 
 sudo apt-get install docker-ce=18.06.1~ce~3-0~raspbian
 
-echo "******************************"
-echo " Setup Git Support"
-echo "******************************"
-
 sudo apt-get install git
-
-echo "******************************"
-echo " Restart"
-echo "******************************"
 
