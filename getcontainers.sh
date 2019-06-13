1b9fbc72460885c643680665ce0aba8ed026320f
root <root@pidev.fritz.box>
2019-06-13 20:55:46 +0100

    repair get containers script

--- getcontainers.sh
+++ getcontainers.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 cd /etc
-mkdir jumajumo
+sudo mkdir jumajumo
 cd jumajumo
 
 sudo git clone https://github.com/jumajumo/dht22-docker-arm.git
 sudo git clone https://github.com/jumajumo/relais-button-docker-arm.git
 sudo git clone https://github.com/jumajumo/relais-switch-on-off-docker-arm.git
+
