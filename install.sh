e76992bcac2cde8ed70e0d79155f162f85946107
jumajumo <45454970+jumajumo@users.noreply.github.com>
2019-07-13 22:10:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo usermod -aG docker pi
 
-sudo apt-get install docker-ce=18.06.1~ce~3-0~raspbian
+sudo apt-get -y install docker-ce=18.06.1~ce~3-0~raspbian
 
-sudo apt-get install git
+sudo apt-get -y install git
 
