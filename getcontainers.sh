bc0e3ed868c657041343de326a48cb4a0376c24a
jumajumo <45454970+jumajumo@users.noreply.github.com>
2020-09-18 08:45:20 +0200

    add distance-sensor docker

--- getcontainers.sh
+++ getcontainers.sh
@@ -7,4 +7,6 @@ cd jumajumo
 sudo git clone https://github.com/jumajumo/dht22-docker-arm.git
 sudo git clone https://github.com/jumajumo/relais-button-docker-arm.git
 sudo git clone https://github.com/jumajumo/relais-switch-on-off-docker-arm.git
+sudo git clone https://github.com/jumajumo/distance-docker-arm.git
+
 
